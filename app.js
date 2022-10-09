49a809b6e3ad3caa9016b735e6679d3de82efc10
iferhatzdemir <ferhatzdemir9@gmail.com>
2022-10-09 19:58:35 +0300

    Template engine uygulandı ejs modulu eklendi

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 const app = express();
-
+//**Template Engine */
+app.set('view engine', 'ejs');
+// ** MIDDLWARE **
+app.use(express.static('public'));
 const port = 3002;
 
 app.get('/', (req, res) => {
-  const blogPost = {
-    title: 'My first blog post',
-    content: 'This is my first blog post',
-    photo: 'https://picsum.photos/200/300',
-    AudioDestinationNode: 'https://www.youtube.com/watch?v=QH2-TGUlwu4',
-  };
-  res.send(blogPost);
+  res.render('index');
 });
-
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
