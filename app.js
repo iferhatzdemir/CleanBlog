9f5e5186336ac9c9a7e8f320ea675f17f2b4bae7
İbrahim Ferhat Özdemir <ferhatzdemir9@gmail.com>
2022-10-21 23:15:36 +0300

    port bug fixed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const postController = require('./controllers/postControllers');
 const pageController = require('./controllers/pageControllers');
 const methodOverride = require('method-override');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //VİEW ENGİNE
 mongoose
